55de535f85e215e434a94fb94928646bffd94f0d
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2020-09-25 12:15:25 -0300

    [style] add on click random render

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.body.appendChild(renderer.domElement);
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(
-  75,
+  90,
   window.innerWidth / window.innerHeight,
   0.1,
   5000
@@ -16,27 +16,33 @@ var camera = new THREE.PerspectiveCamera(
 camera.position.z = -5000;
 camera.lookAt(scene.position);
 
-var light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.x = 0;
-light.position.y = 0;
+var light = new THREE.DirectionalLight(0xffffff, 0.6);
+light.position.x = -1;
+light.position.y = -1;
 light.position.z = -1;
 scene.add(light);
 
 const geometries = [
-  new THREE.BoxBufferGeometry(15, 15, 15),
-  new THREE.ConeBufferGeometry(10, 20, 30),
-  new THREE.TorusBufferGeometry(5, 3, 16, 100)
+  new THREE.BoxBufferGeometry(30, 30, 30),
+  new THREE.ConeBufferGeometry(30, 30, 30),
+  new THREE.TorusBufferGeometry(30, 10, 16, 100),
+  new THREE.CircleBufferGeometry(30, 32),
+  new THREE.CylinderBufferGeometry(30, 30, 90, 100),
+  new THREE.DodecahedronBufferGeometry(30),
+  new THREE.IcosahedronBufferGeometry(30),
+  new THREE.OctahedronBufferGeometry(30),
+  new THREE.SphereBufferGeometry(30, 30, 30),
+  new THREE.TetrahedronBufferGeometry(30),
+  new THREE.TorusKnotBufferGeometry(30, 5, 100, 16),
 ];
 
 let hue = 0;
 let shapes = [];
 
 const addShape = function (x, y) {
-  var geometry = geometries[Math.floor(Math.random() * geometries.length)];
-
   const material = new THREE.MeshLambertMaterial({
     color: 0xffffff,
-    emissive: new THREE.Color("hsl(" + hue + ", 100%, 50%)")
+    emissive: new THREE.Color("hsl(" + hue + ", 100%, 40%)")
   });
 
   hue += 1;
@@ -68,32 +74,14 @@ var animate = function () {
 
 animate();
 
-// ativar onClick
-// let isMouseDown = false;
-
-// desativar onClick
 let isMouseDown = true;
 
-document.addEventListener("mousedown", function (event) {
-  isMouseDown = true;
-});
-
-document.addEventListener("mouseup", function (event) {
-  isMouseDown = false;
-});
+var geometry = geometries[9];
 
 document.addEventListener("mousemove", function (event) {
   if (isMouseDown) {
     addShape(event.pageX, event.pageY);
   }
-});
-
-document.addEventListener("touchstart", function (event) {
-  isMouseDown = true;
-});
-
-document.addEventListener("touchend", function (event) {
-  isMouseDown = false;
 });
 
 document.addEventListener("touchmove", function (event) {
@@ -102,6 +90,12 @@ document.addEventListener("touchmove", function (event) {
   }
   event.preventDefault();
 });
+
+document.addEventListener("click", () => {
+  if (isMouseDown) {
+    geometry = geometries[Math.floor(Math.random() * geometries.length)];
+  }
+})
 
 window.addEventListener("resize", function () {
   camera.aspect = window.innerWidth / window.innerHeight;
