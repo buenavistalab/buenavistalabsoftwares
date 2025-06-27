6fb2b1018cf6eb1aa0b4c3ae1a22fe70cc5789b1
Saulo Filho <saulosilvaf@gmail.com>
2025-06-27 14:37:59 -0300

    add terrain generator

--- art/terraingenerator/script.js
+++ art/terraingenerator/script.js
@@ -1,77 +1,136 @@
-class Blob {
+var cols, rows;
+var scl = 20;
+var w = 2500;
+var h = 1000;
+var flying = 0;
+var terrain = [];
 
-  constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      let angle = random(0, 2*PI);
-      this.xspeed = random(-30, 15)*Math.cos(angle);
-      this.yspeed = random(-30, 15)*Math.sin(angle);
-      this.r = random(60, 200);
+var sliderPI;
+var sliderVelocidade;
+var sliderOnda;
+var sliderTamOndaMenos;
+var sliderTamOndaMais;
+var sliderStrokeW;
+
+function setup() {
+  var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  canvas.parent('myContainer');
+  cols = w/scl;
+  rows = h/scl;   
+  for (var x = 0; x < cols; x++) {
+    terrain[x] = [];
+    for (var y = 0; y < rows; y++) {
+      terrain[x][y] = 0; //specify a default value for now
+    }
   }
+  // slider pi
+  sliderPI = createSlider(1, 4, 1, 0.001);
+  sliderPI.parent('myInputs');
+  sliderPI.class('inputs');
+  let text1 = createP('PI');
+  text1.parent('myTexts');
+  text1.class('texts');
+  // slider stroke weight
+  sliderStrokeW = createSlider(0.01, 9, 9, 0.01);
+  sliderStrokeW.parent('myInputs');
+  sliderStrokeW.class('inputs');
+  let text6 = createP('STROKE WEIGHT');
+  text6.parent('myTexts');
+  text6.class('texts');
+  // slider velocidade
+  sliderVelocidade = createSlider(0.01, 1, 0.01, 0.01);
+  sliderVelocidade.parent('myInputs');
+  sliderVelocidade.class('inputs');
+  let text2 = createP('SPEED');
+  text2.parent('myTexts');
+  text2.class('texts');
+  // slider onda
+  sliderOnda = createSlider(0, 1, 0, 0.01);
+  sliderOnda.parent('myInputs');
+  sliderOnda.class('inputs');
+  let text3 = createP('WAVE DEFORMITY');
+  text3.parent('myTexts');
+  text3.class('texts');
+  // slider tamanho da onda mais
+  sliderTamOndaMais = createSlider(10, 900, 10, 0.01);
+  sliderTamOndaMais.parent('myInputs');
+  sliderTamOndaMais.class('inputs');
+  let text4 = createP('WAVE SIZE (+)');
+  text4.parent('myTexts');
+  text4.class('texts');
+  // slider tamanho da onda menos
+  sliderTamOndaMenos = createSlider(-200, 0, 0, 0.01);
+  sliderTamOndaMenos.parent('myInputs');
+  sliderTamOndaMenos.class('inputs');
+  let text5 = createP('WAVE SIZE (-)');
+  text5.parent('myTexts');
+  text5.class('texts');
+  saveButton = createButton('SAVE IMG');
+  saveButton.mousePressed(saveImg);
+  saveButton.parent('but');
+}
 
-  update() {
-      this.x += this.xspeed;
-      this.y += this.yspeed;
-      if(this.x > width || this.x < 0) this.xspeed *= -1;
-      if(this.y > height || this.y < 0) this.yspeed *= -1;
+function saveImg() {
+  save('terrain.png');
+}
+
+function draw() {
+//pi
+var piPI = sliderPI.value();
+//velocidade
+var velo = sliderVelocidade.value();
+//onda
+var onda = sliderOnda.value();
+//tamanho da onda menos
+var ondaMenos = sliderTamOndaMenos.value();
+//tamanho da onda mais
+var ondaMais = sliderTamOndaMais.value();
+//stroke weight
+var sW = sliderStrokeW.value();
+
+  flying -= velo; //velocidade
+  var yoff = flying;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, ondaMenos, ondaMais); //tamanho das ondas: -100, 100
+      xoff += onda;
+    }
+    yoff += onda; //deformidade da onda, o valor inicial de xoff e yoff é +=0.1
   }
+  
+  background(0);
+  //translate(0, 0, 0);
+  //normalMaterial();
+  //fill(random(255),random(255),random(255));
+  fill(0);
+  push();
+  rotateX(PI/piPI);
+  stroke(random(255),random(255),random(255));
+  //stroke(255);
+  strokeWeight(sW);
+  translate(-w/2, -h/2);
 
-  show() {
-      ellipse(this.x, this.y, this.r*1, this.r*1);
+  for (var y = 0; y < rows-1; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (var x = 0; x < cols; x++) {
+      vertex(x*scl, y*scl, terrain[x][y]);
+      vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
+    }
+    endShape();
   }
 }
 
-var blobs = []
-var slider;
-var slider2;
-var slider3;
-var mask;
+function mousePressed (){
+  windowResized();
+}
 
-function setup() {
-    let canvas = createCanvas(400, 400);
-    canvas.parent('myContainer');
-    colorMode(HSB);
-    for(i = 0; i < 10; i++) blobs.push(new Blob(random(0, width), random(0, height)));
-    slider = createSlider(-200, 200, 0, 0.01);
-    slider.parent('myInputs');
-    slider.class('inputs');
-    slider2 = createSlider(-100, 100, 50, 0.01);
-    slider2.parent('myInputs');
-    slider2.class('inputs');
-    slider3 = createSlider(-100, 100, 10, 0.01);
-    slider3.parent('myInputs');
-    slider3.class('inputs');
-  }
-  
-function draw() {
-    var val = slider.value();
-    var val2 = slider2.value();
-    var val3 = slider3.value();
-
-    loadPixels();
-    for(x = 0; x < width; x++) {
-        for(y = 0; y < height; y++) {
-            let sum = val;
-            for(i = 0; i < blobs.length; i++) {
-                let xdif = x - blobs[i].x;
-                let ydif = y - blobs[i].y;
-                let d = sqrt((xdif*xdif) + (ydif*ydif));
-                sum += random(val2, val3) * blobs[i].r/d;
-            }
-            set(x, y, color(sum, 255, 255));
-        }
-    }
-    updatePixels();
-
-    for(i = 0; i < blobs.length; i++) {
-        blobs[i].update();
-    }
-
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 $(document).ready(function() {
-    $("button").click(function() {
-      $("span").toggle();
-    })
+  $("button").click(function() {
+    $("span").toggle();
   })
-  
+})
